c1e6fdf6d4b18482ab3710b686eb38375a832885
Doston <dostonbek8747@gmial.com>
2025-07-15 23:09:44 +0500

    first commit

--- llama_chat.py
+++ llama_chat.py
@@ -5,7 +5,7 @@ response = requests.post(
     "http://localhost:11434/api/generate",
     json={
         "model": "llama3",
-        "prompt": "You can help men learn python ?",
+        "prompt": "You can help me learn python ?",
         "stream": False
     }
 )
